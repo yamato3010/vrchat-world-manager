1627f3f04b40270107464e065920dd943f0e221b
yamato3010 <yama3to10yama3@gmail.com>
2026-01-10 15:23:32 +0900

    ci: 👷 GitHub ActionsのJob Summariesにカバレッジレポートを出力するように

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         // カバレッジ設定
         coverage: {
             provider: 'v8',
-            reporter: ['text', 'json', 'html'],
+            reporter: ['text', 'json', 'html', 'json-summary'],
             exclude: [
                 'node_modules/',
                 '__tests__/',
